dab9ab382f29c415003f4d7b7fa92c91903a523c
amathsys <26331890+amathsys@users.noreply.github.com>
2024-06-24 14:14:06 -0700

    Update gs_debug.sh

--- src/gspack/templates/gs_debug.sh
+++ src/gspack/templates/gs_debug.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 python3.10 -m pip uninstall -y gspack
-git clone https://github.com/aksholokhov/gspack
+git clone https://github.com/amathsys/gspack
 cd gspack || exit
 python3.10 setup.py develop
