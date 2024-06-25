86b9e790dd97cf2579ab73ed37d9de0cd97294d8
amathsys <26331890+amathsys@users.noreply.github.com>
2024-06-25 11:09:47 -0700

    Update __about__.py
    Changed repository from aksholokhov to amathsys

--- src/gspack/__about__.py
+++ src/gspack/__about__.py
@@ -24,7 +24,7 @@ __summary__ = "Assignment Packager for the Gradescope Autograder."
 __long_description__ = ("This package is designed to simplify setting" +
                         " up programming assignments for Gradescope Autograder." +
                         " See README.md for details and examples.")
-__uri__ = "https://github.com/aksholokhov/gspack"
+__uri__ = "https://github.com/amathsys/gspack"
 
 __version__ = "0.2.12"
 
